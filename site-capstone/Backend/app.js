009745a0ac3149836f0082b6215bc64f910bac1d
kiara-bermudez <kiarab@tamu.edu>
2022-07-18 17:08:45 -0500

    Added login and register post requests. Added auth routes to app

--- site-capstone/Backend/app.js
+++ site-capstone/Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -8,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+
+// Routes
+app.use("/auth", authRoutes);
 
 // Health endpoint
 app.get("/",  (req, res, next) => {
