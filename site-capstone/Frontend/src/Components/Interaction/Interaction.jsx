851b3d5cba8375f2a58d7cd327e8a39d2891e415
reyesenamorado97 <reyesenamorado@googlemail.com>
2022-07-19 16:52:52 -0400

    Search bars perfrom API request to get both medication  rxcui's

--- site-capstone/Frontend/src/Components/Interaction/Interaction.jsx
+++ site-capstone/Frontend/src/Components/Interaction/Interaction.jsx
@@ -6,21 +6,49 @@ export default function Interaction({ }) {
     
     const [form1, setForm1] = useState({
         medication1: "",
-        rxcui1: "",
+        rxcui1: 0,
     });
 
     const [form2, setForm2] = useState({
         medication2: "",
-        rxcui2: "",
+        rxcui2: 0,
     });
 
     const [errors, setErrors] = useState({});
 
-    const handleOnInputChange = (event) => {
-       
-        setForm((f) => ({ ...f, [event.target.name]: event.target.value }));
-        
+    const handleOnInputChange1 = (event) => { 
+        setForm1((f) => ({ ...f, [event.target.name]: event.target.value })); 
     }
+
+    const handleOnInputChange2= (event) => { 
+        setForm2((f) => ({ ...f, [event.target.name]: event.target.value })); 
+    }
+
+    useEffect(() => {
+        axios.get("https://rxnav.nlm.nih.gov/REST/rxcui.json?name=" + form1.medication1 + "&search=1")
+            .then((response) => {
+                console.log(response.data.idGroup.rxnormId[0])
+                setForm1({...form1 ,rxcui1: response.data.idGroup.rxnormId[0]})
+            })
+            .catch((error) => {
+                setForm1({...form1 ,rxcui1: 0})
+            })
+        
+    }, [form1.medication1])
+
+    useEffect(() => {
+        axios.get("https://rxnav.nlm.nih.gov/REST/rxcui.json?name=" + form2.medication2 + "&search=1")
+            .then((response) => {
+                console.log(response.data.idGroup.rxnormId[0])
+                setForm2({...form2 ,rxcui2: response.data.idGroup.rxnormId[0]})
+            })
+            .catch((error) => {
+                setForm2({...form2 ,rxcui2: 0})
+            })
+        
+    }, [form2.medication2])
+
+    
 
     return (
         <div className="container px-4 px-lg-5 h-100">
@@ -39,25 +67,63 @@ export default function Interaction({ }) {
           }
                 {/* ROW 1 */}
                 <div className="form-row row">
-                    <div className="col-md-6 mb-3" >                           
+                    <div className="col-md-4 mb-3" >                           
                         <label className="form-label"> Medication Name</label>
-                        <input name="medicationName" type="text" className="form-control" placeholder="Medication" value={form1.medication1} onChange={handleOnInputChange} />
+                        <input name="medication1" type="text" className="form-control" placeholder="Medication" value={form1.medication1} onChange={handleOnInputChange1} />
+                            <div>
+                                {form1?.rxcui1 !== 0 && form1.medication1?.length !== 0 ?
+                                    
+                                    <div className="success">
+                                        {form1.medication1} is a valid medication
+                                    </div>
+                                    :
+                                    <div >
+                                        &nbsp;
+                                    </div>
+                                }
+
+                                {form1?.rxcui1 === 0 && form1.medication1?.length !== 0 ?
+                                    <div className="error">Please enter a a valid medication!</div>
+                                    :
+                                    <div >
+                                        &nbsp;
+                                    </div>
+                                    
+                                } 
+
+                            </div>   
+                        </div>
+
+                        <div className="col-md-4 mb-3" >                           
+                        <label className="form-label"> Medication Name</label>
+                        <input name="medication2" type="text" className="form-control" placeholder="Medication" value={form2.medication2} onChange={handleOnInputChange2} />
                         <div>
-                    {form1.rxcui1 !== 0 && form1.medication1.length !== 0 &&
+                    {form2.rxcui2 !== 0 && form2.medication2.length !== 0 ?
                                 <div className="success">
-                                    {form.medicationName} is a valid medication
-                                </div>
+                                    {form2.medication2} is a valid medication
+                                    </div>
+
+                                    :
+                                    
+                                    <div >
+                                        &nbsp;
+                                    </div>
                     }
 
-                    {form1.rxcui1 === 0 && form1.medication1.length !== 0 &&
-                        <div className="error">Please enter a a valid medication!</div>
+                    {form2.rxcui2 === 0 && form2.medication2.length !== 0 ?
+                                    <div className="error">Please enter a a valid medication!</div>
+                                    :
+                                    <div >
+                                    &nbsp;
+                                </div>
                             } 
                         </div>                        
+                        </div>
+                        <div className="align-self-baseline text-center mt-4 mb-5">
+                        <a className="btn btn-dark btn-x1 row " >Compare</a> 
                     </div>
                 </div>
 
-
-                
             </form>
         </div>
     </div>
