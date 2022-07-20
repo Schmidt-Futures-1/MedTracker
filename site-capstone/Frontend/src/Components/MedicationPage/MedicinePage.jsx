5b1f3e52ebfff20014ed8077e2f94ec09b3b3d0e
reyesenamorado97 <reyesenamorado@googlemail.com>
2022-07-20 13:44:49 -0400

    Medicine cards are now appearing, but allignment is wrong

--- site-capstone/Frontend/src/Components/MedicationPage/MedicinePage.jsx
+++ site-capstone/Frontend/src/Components/MedicationPage/MedicinePage.jsx
@@ -1,12 +1,49 @@
 import "./MedicinePage.css"
 import MedicineCard from "../MedicineCard/MedicineCard"
+import apiClient from "../../services/apiClient";
+import { useState, useEffect } from "react";
 
-export default function MedicinePage({
-    user,
-    setUser
-
-})
+export default function MedicinePage({ user, setUser })
 {
+
+    // State variables ----------------------------------------------------------------------------
+    const [medications, setMedications] = useState([]);
+    const [errors, setError] = useState([]);
+    const [isLoading, setIsLoading] = useState();
+
+
+    // Functions ----------------------------------------------------------------------------------
+
+    useEffect(() => {
+        const fetchMedications = async () => {
+            const { data, error} = await apiClient.fetchUserMedications();
+            console.log("fetch meds data", data);
+            if (data) {
+                setMedications(data.medications);
+                console.log("after setting meds", medications)
+                setError(null);
+            } 
+            if (error) {
+                setError(error);
+            }
+
+            
+
+            setIsLoading(false);
+        }
+        
+        if (user?.email) {
+            setIsLoading(true);
+            setError(null);
+            fetchMedications();
+        } else {
+            setIsLoading(false);
+        }
+      }, [user]);
+
+
+
+    console.log("med page meds", medications)
 
     return(
         <div className="container">
@@ -23,7 +60,11 @@ export default function MedicinePage({
             </div>
 
             {/* Medicine cards */}
-            <MedicineCard/>
+            {isLoading? <h1>Loading...</h1> :
+                medications?.map((item, idx) => (
+                    <MedicineCard key={idx} medication={item}/>
+                ))
+            }
 
         </div>
 
--- site-capstone/Frontend/src/Components/MedicineCard/MedicineCard.jsx
+++ site-capstone/Frontend/src/Components/MedicineCard/MedicineCard.jsx
@@ -1,22 +1,32 @@
 import "./MedicineCard.css"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 
-export default function MedicineCard(){
-    return(
+export default function MedicineCard({medication}){
+    return (
+        
         <div className="row">
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Medicine Name</h5>
-                            <p className="card-text">Strength: ##</p>
-                            <p className="card-text">Pills Left: ##</p>
-                            <a href="#" className="btn btn-dark btn-space">Refill</a>
-                            <a href="#" className="btn btn-dark btn-space">Edit</a>
-                            <a href="#" className="btn btn-dark btn-space">Delete</a>
-                        </div>          
-                    </div>
+            <div className="col-sm-3">
+                
+
+                {/* Single card starts here */}
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{medication.name }</h5>
+                        <p className="card-text">Strength: {medication.strength} { medication.units}</p>
+                        <p className="card-text">Pills Left: {medication.current_pill_count} / {medication.total_pill_count }</p>
+                        <a href="#" className="btn btn-dark btn-space">Refill</a>
+                        <a href="#" className="btn btn-dark btn-space">Edit</a>
+                        <a href="#" className="btn btn-dark btn-space">Delete</a>
+                    </div>          
+                    
                 </div>
+                
             </div>
+            
+        </div>
+        
        )
     }
 
