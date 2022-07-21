f966cae75e5be551148907ffcc6959927d286137
kiara-bermudez <kiarab@tamu.edu>
2022-07-21 16:05:28 -0500

    Added error components

---
+++ site-capstone/Frontend/src/Components/Error Pages/AccessForbidden.jsx
@@ -0,0 +1,8 @@
+export default function AccessForbidden(){
+    return(
+    <div className="access-forbidden">
+        <h1>Access Forbidden</h1>
+        <a className="btn btn-dark btn-x1" href="/login">Login</a>
+    </div>
+    )   
+}
