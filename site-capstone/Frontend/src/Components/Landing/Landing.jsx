b8c74406e70741d9b9a1f1d1c17616c48ce2ba4f
reyesenamorado97 <reyesenamorado@googlemail.com>
2022-07-24 21:21:57 -0400

    Added conditional rendering for login and signup buttons on landing page

--- site-capstone/Frontend/src/Components/Landing/Landing.jsx
+++ site-capstone/Frontend/src/Components/Landing/Landing.jsx
@@ -2,7 +2,9 @@ import "./Landing.css"
 import { Link } from "react-router-dom"
 
 
-export default function Landing(){
+export default function Landing({ user }) {
+    
+    console.log(user?.firstName)
     return(
     <div className="container px-4 px-lg-5 h-100">
         <div className="headingRow row gx-4 gx-lg-5 h-100">
@@ -12,9 +14,16 @@ export default function Landing(){
             </div>
             <div className="col-lg-8 align-self-baseline">
                 <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <Link className="btn btn-dark btn-x1 mb-1" to="/register">Sign Up</Link><br/>
-                <Link className="btn btn-dark btn-x1" to="/login">Login</Link> 
+                    {user?.firstName === undefined ?
+                        <div>
+                            <Link className="btn btn-dark btn-x1 mb-1" to="/register">Sign Up</Link><br />
+                            <Link className="btn btn-dark btn-x1" to="/login">Login</Link>
+                        </div>
+                        :
+                        ""
+                    }
             </div>
+                
         </div>
     </div>
 
