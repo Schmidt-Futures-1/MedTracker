6694ffadf9ee60dd854b352d1849bd44e726217e
kiara-bermudez <kiarab@tamu.edu>
2022-08-02 16:31:28 -0500

    Deployed to Surge

--- site-capstone/Frontend/constants.js
+++ site-capstone/Frontend/constants.js
@@ -1,5 +1,5 @@
-export const PRODUCTION_API_BASE_URL = "heroku link goes here"
+export const PRODUCTION_API_BASE_URL = "https://medtrackerv1.herokuapp.com"
 
 export const DEVELOPMENT_API_BASE_URL = "http://localhost:3001"
 
-export const API_BASE_URL = DEVELOPMENT_API_BASE_URL;
+export const API_BASE_URL = PRODUCTION_API_BASE_URL;
