c3251f5f3f71f44092f248eb588c45592262b423
reyesenamorado97 <reyesenamorado@googlemail.com>
2022-08-09 14:50:45 -0400

    Cleaned up navbar

--- site-capstone/Frontend/src/Components/Navbar/Navbar.jsx
+++ site-capstone/Frontend/src/Components/Navbar/Navbar.jsx
@@ -16,45 +16,43 @@ export default function NavBar({user, setUser}){
 
     return(
 
-        <nav className="navbar navbar-expand-lg bg-dark bg-color">
+    <nav className="navbar navbar-expand-lg bg-dark bg-color">
       <div className="container-fluid">
+
         <div className="navbar-brand"><Link to="/"><img src={logo} alt="codepath logo"/></Link></div>
+
         <button className="navbar-toggler bg-color" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon custom-toggler bg-color"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               {user?.email?(
                 <Link className="nav-link" to="/dashboard">Dashboard</Link>
-              ):(
-                ""
-              )}
+              ):("")}
             </li>
+
             <li className="nav-item">
               {user?.email?(
                 <Link className="nav-link" to="/create">Add Medication</Link>
-              ):(
-                ""
-              )}
+              ):("")}
             </li>
+
             <li className="nav-item">
               {user?.email?(
                 <Link className="nav-link" to="/cabinet">Medicine Cabinet</Link>
-              ):(
-                ""
-              )}
-              </li>
+              ):("")}
+            </li>
 
-              {/* Nav link for interaction tracker */}
-              <li className="nav-item">
+            {/* Nav link for interaction tracker */}
+            <li className="nav-item">
               {user?.email?(
                 <Link className="nav-link bg-color" to="/interaction">Interaction Checker</Link>
-              ):(
-                ""
-              )}
+              ):("")}
             </li>
           </ul>
+
           <ul className="navbar-nav ml-auto bg-color">
             <li className="nav-item ">
                 <div className="nav-link">
@@ -62,10 +60,9 @@ export default function NavBar({user, setUser}){
                         ""
                   ) : (
                       <Link to="/login">
-                          <button className="btn-secondary login-sizing ">Login
-                        </button> 
-                        </Link>
-                        )}
+                          <button className="btn-secondary login-sizing ">Login</button> 
+                      </Link>
+                    )}
                 </div>
             </li>
             <li className="nav-item">
@@ -73,15 +70,13 @@ export default function NavBar({user, setUser}){
                      {user?.email?(
                         <button onClick={handleLogout} className="btn-secondary bg-color">Sign Out</button> 
                   ) : (
-                    <Link to="/register">
-                          <button className="btn-secondary bg-color">
-                            Sign Up
-                        </button>  
-                        </Link>
-                        )}
-                  </div>
+                      <Link to="/register">
+                          <button className="btn-secondary bg-color">Sign Up</button>  
+                      </Link>
+                          )}
+                </div>
             </li>
-        </ul>
+          </ul>
         </div>
       </div>
     </nav>
